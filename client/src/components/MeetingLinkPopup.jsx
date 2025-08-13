845294a65f7b15bb6cb47834b8924cdf3c40e463
Jaikarans <jaikrns@gmail.com>
2025-08-13 09:30:36 +0530

    typo: fix tab indentaion size

--- client/src/components/MeetingLinkPopup.jsx
+++ client/src/components/MeetingLinkPopup.jsx
@@ -30,52 +30,52 @@ import { useEffect, useRef } from "react";
 
     return (
       <HStack wrap="wrap" width={20} gap="4">
-          <Dialog.Root
-            open={isPopupOpen}
-            onOpenChange={(e) => {
-              if (typeof e === 'boolean') {
-                setIsPopupOpen(e);
-              } else if (typeof e === 'object' && 'open' in e) {
-                setIsPopupOpen(e.open);
-              }
-            }}
-            key={"center"}
-            placement={"center"}
-            css={style.root}
+        <Dialog.Root
+          open={isPopupOpen}
+          onOpenChange={(e) => {
+            if (typeof e === 'boolean') {
+              setIsPopupOpen(e);
+            } else if (typeof e === 'object' && 'open' in e) {
+              setIsPopupOpen(e.open);
+            }
+          }}
+          key={"center"}
+          placement={"center"}
+          css={style.root}
 
-          >
-            <Portal>
-              <Dialog.Backdrop />
-              <Dialog.Positioner>
-                <Dialog.Content css={style.content}
-                  onFocusCapture={(e) => e.target.blur()} 
-                >
-                  <Dialog.Header css={style.header}>
-                    Here's your joining Information
-                    <Dialog.CloseTrigger asChild css={style.closeTrigger}>
-                      <CloseButton />
-                    </Dialog.CloseTrigger>
-                  </Dialog.Header>
-                  <Dialog.Body css={style.body} >
-                    <Text>
-                      Send this link to person you want to meet with. Make sure that you save it so that you can use it later, too.
-                    </Text>
-                    <Clipboard.Root ref={copyBtnRef} value={meetingLink}>
-                      <Group flexGrow="1" borderRadius="4px" w="full" bg="surface-container" color="on-surface">
-                        <Clipboard.ValueText flexGrow="1" fontWeight="400" fontSize="1rem" gap="0.5rem" pl=".75rem" />
-                        <Spacer />
-                        <ClipboardIconButton />
-                      </Group>
-                    </Clipboard.Root>
-                    <Spacer />
-                    <Spacer />
-                  </Dialog.Body>
-                  
-                </Dialog.Content>
-              </Dialog.Positioner>
-            </Portal>
-          </Dialog.Root>
-    </HStack>
+        >
+          <Portal>
+            <Dialog.Backdrop />
+            <Dialog.Positioner>
+              <Dialog.Content css={style.content}
+                onFocusCapture={(e) => e.target.blur()} 
+              >
+                <Dialog.Header css={style.header}>
+                  Here's your joining Information
+                  <Dialog.CloseTrigger asChild css={style.closeTrigger}>
+                    <CloseButton />
+                  </Dialog.CloseTrigger>
+                </Dialog.Header>
+                <Dialog.Body css={style.body} >
+                  <Text>
+                    Send this link to person you want to meet with. Make sure that you save it so that you can use it later, too.
+                  </Text>
+                  <Clipboard.Root ref={copyBtnRef} value={meetingLink}>
+                    <Group borderRadius="4px" w="full" bg="surface-container" color="on-surface-variant">
+                      <Clipboard.ValueText fontWeight="medium" gap="0.5rem" pl=".75rem" />
+                      <Spacer />
+                      <ClipboardIconButton />
+                    </Group>
+                  </Clipboard.Root>
+                  <Spacer />
+                  <Spacer />
+                </Dialog.Body>
+                
+              </Dialog.Content>
+            </Dialog.Positioner>
+          </Portal>
+        </Dialog.Root>
+      </HStack>
 
     );
   }
